81836b2a768fdf2b1bad70486af8e54a3f1d6293
Kim_SeulBee <stella_62@naver.com>
2025-07-20 06:50:39 +0900

    최종 커밋: SQL 파일만 포함

--- dist/lib/prismaClient.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.prismaClient = void 0;
-const client_1 = require("@prisma/client");
-const prisma = global.prisma || new client_1.PrismaClient();
-if (process.env.NODE_ENV !== 'production') {
-    global.prisma = prisma;
-}
-exports.prismaClient = prisma;
